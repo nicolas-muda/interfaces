d8429e6ca3d1b5a8109d7193e5a5bfa957a72295
Nicolas Muda <nicolasmuda@live.com.ar>
2023-07-08 16:54:36 -0300

    se arreglaron unas cuantas cosas

--- js/noticias.js
+++ js/noticias.js
@@ -1,20 +1,20 @@
 let noticia1 = document.getElementById("cronograma del quirofano movil");
 let noticia2 = document.getElementById("veterinarios recomiendan no humanizar a sus mascotas");
-let noticia3 = document.getElementById("se realizara esterilización en los centros");
+let noticia3 = document.getElementById("Esterilizaciones gratuitas");
 let noticia4 = document.getElementById("continua la reubicacion de los animales rescatados");
-let noticia5 = document.getElementById("rescate en tres arroyos");
+let noticia5 = document.getElementById("Se realizo un rescate en tres arroyos");
 let noticia6 = document.getElementById("campaña crowfunding");
 
 let input = document.getElementById("buscador");
 
-let noticias = [noticia1,noticia2,noticia3,noticia4,noticia5,noticia6];
+let noticias = [noticia1, noticia2, noticia3, noticia4, noticia5, noticia6];
 
 input.addEventListener("input", (e) => {
     noticias.forEach(noticia => {
-        if(noticia.id.includes(input.value)){
+        if (noticia.id.includes(input.value)) {
             noticia.style.display = 'block';
         }
-        else{
+        else {
             noticia.style.display = 'none';
         }
     });
